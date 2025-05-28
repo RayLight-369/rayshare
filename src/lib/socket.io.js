942e9b2aec6cd7054f6f6d4e31eba71723ff879f
RayLight-369 <abdulrafay.designs@gmail.com>
2025-05-28 14:52:00 +0500

    update

--- src/lib/socket.io.js
+++ src/lib/socket.io.js
@@ -1,5 +1,5 @@
 import { io } from "socket.io-client";
 
-export const socket = io( 'http://localhost:3030', {
+export const socket = io( 'https://rayshare-server.onrender.com/', {
   autoConnect: false
 } );
